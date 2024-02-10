e7c6880fb0b14a85a699ae1720efee5e9501cd29
josiahwiebe <josiah@collectif.co>
2024-02-10 11:44:18 -0600

    fix: type error

--- app/(blog)/blog/page.tsx
+++ app/(blog)/blog/page.tsx
@@ -14,26 +14,28 @@ export default async function BlogPage() {
     <>
       <PageHeader title='Blog' subtitle='This and that.' />
       <div className='page-content'>
-        {posts.map(post => (
-          <article key={post.slug} className='flex flex-col space-y-4'>
-            <div className='flex flex-col space-y-2'>
-              <Link href={post.url} passHref>
-                <h2 className='max-w-[80%] text-2xl font-heading font-bold leading-normal sm:text-3xl md:text-3xl'>
-                  {post.frontMatter.title}
-                </h2>
-              </Link>
-              {post.frontMatter.date && (
-                <p className='text-sm text-slate-600 dark:text-slate-400'>{formatDate(post.frontMatter.date)}</p>
+        {posts.map(post =>
+          post ? (
+            <article key={post.slug} className='flex flex-col space-y-4'>
+              <div className='flex flex-col space-y-2'>
+                <Link href={post.url} passHref>
+                  <h2 className='max-w-[80%] text-2xl font-heading font-bold leading-normal sm:text-3xl md:text-3xl'>
+                    {post.frontMatter.title}
+                  </h2>
+                </Link>
+                {post.frontMatter.date && (
+                  <p className='text-sm text-slate-600 dark:text-slate-400'>{formatDate(post.frontMatter.date)}</p>
+                )}
+              </div>
+              {post.frontMatter.excerpt && (
+                <p className='text-slate-600 dark:text-slate-400'>{post.frontMatter.excerpt}</p>
               )}
-            </div>
-            {post.frontMatter.excerpt && (
-              <p className='text-slate-600 dark:text-slate-400'>{post.frontMatter.excerpt}</p>
-            )}
-            <div className='py-8 md:py-10 lg:py-12'>
-              <hr className='border-slate-100 dark:border-slate-700' />
-            </div>
-          </article>
-        ))}
+              <div className='py-8 md:py-10 lg:py-12'>
+                <hr className='border-slate-100 dark:border-slate-700' />
+              </div>
+            </article>
+          ) : null
+        )}
       </div>
     </>
   )
